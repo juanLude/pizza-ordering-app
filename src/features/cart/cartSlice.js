6ef4ac7d158474eeb71535bde7fdfc51ce2393f6
Juan Ludevid <jmludevid@hotmail.com>
2024-02-02 06:12:24 +1100

    update store.js

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -21,9 +21,31 @@ const cartSlice = createSlice({
       //payload = newItem
       state.cart.push(action.payload);
     },
-    deleteItem(state, action) {},
-    increaseItemQuantity(state, action) {},
-    decreaseItemQuantity(state, action) {},
-    clearCart(state, action) {},
+    deleteItem(state, action) {
+      //payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
   },
 });
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
